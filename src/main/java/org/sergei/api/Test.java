e9991bde7d74620ec185d5da3eb560dfd34772d9
sergei-doroshenko <6823298@gmail.com>
2016-06-07 13:16:17 +0300

    Initial commit.

---
+++ src/main/java/org/sergei/api/Test.java
@@ -0,0 +1,12 @@
+package org.sergei.api;
+
+/**
+ * Created by Sergei_Doroshenko on 6/7/2016.
+ */
+public class Test
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello");
+    }
+}
